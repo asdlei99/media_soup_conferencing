de7d486fe76630c144d6e9a5c0a9e851f23dceec
anil <anil235476@gmail.com>
2019-04-20 19:25:05 +0530

    redesign of server

--- server/index_websocket.js
+++ server/index_websocket.js
@@ -2,157 +2,35 @@ const config = require('./config');
 const wsServer = require('./websocket_server'); 
 const media_soup_server = require('./media_soup_server');
 
- class _room_handler{
-   constructor(){
-    this.rooms = new Map();
-   }
-
-   is_room_exists(roomId){
-     return this.rooms.has(roomId);
-   }
-
-   get_room_handle(roomId){
-     return this.rooms.get(roomId);
-   }
-
-   save_room(roomId, room){
-     this.rooms.set(roomId, room);
-   }
-  
-   delete_room(roomId){
-     this.rooms.delete(roomId);
-   }
-
- };
-
-let room_handler = new _room_handler();
-
-function handle_room_join_request(peerName, roomId){
-  
-      if (room_handler.is_room_exists(roomId)) {
-        console.log("existing room found for request from "+ peerName);
-        return room_handler.get_room_handle(roomId);
-      } else {
-        console.log("creating new room for "+ peerName)
-        //room = mediaServer.Room(config.mediasoup.mediaCodecs);
-        let room = media_soup_server.create_room(config.mediasoup.mediaCodecs);
-        room_handler.save_room(roomId, room);
-        room.on('close', () => {
-          room_handler.delete_room(roomId);
-        });
-        return room;
-      }
-}
-
-
+ 
 function handle_conference(request){
     let ext_connection = request.accept(null, request.origin);
+    let key = request.key;
     let roomId = request.resourceURL.query.roomId;
     let peerName = request.resourceURL.query.peerName;
-    let room = null;
-    let mediaPeer = null;
+    let peer = media_soup_server.save_peer(key, peerName, roomId, ext_connection);
     ext_connection.on('message', function (data) {
         let response = JSON.parse(data.utf8Data);
         if(response.type == "request_room_join"){
-          room = handle_room_join_request(peerName, roomId);
-
+          room = media_soup_server.handle_room_join_request(peerName, roomId);
         }
         else if(response.type == 'mediasoup-request'){
+         
            
             let request = response.request;//todo: handle this
             let what = request.method; //todo: handle this
             let id = response.id;
             console.log(request, what);
-            switch (what) {
-
-                case 'queryRoom':
-                  room.receiveRequest(request)
-                    .then((response) => {
-                        ext_connection.send(JSON.stringify(
-                        {'type':"queryRoomResponse",
-                        'id':id,
-                        'm':response
-                        }));
-                    })
-                    .catch((error) =>{
-                        console.log("handle this error", error);
-                        ext_connection.send(JSON.stringify(
-                          {'type':"queryRoomResponseError",
-                          'id':id,
-                          'm':error.toString()
-                          }));
-                     //cb(error.toString())
-                    });
-                  break;
-          
-                case 'join':
-                  room.receiveRequest(request)
-                    .then((response) => {
-                      // Get the newly created mediasoup Peer
-                      mediaPeer = room.getPeerByName(peerName);
-          
-                      media_soup_server.handleMediaPeer(mediaPeer, ext_connection);
-          
-                      // Send response back
-                      ext_connection.send(JSON.stringify({
-                          'type':"join_response",
-                          'id':id,
-                          m:response
-                      }));
-                    })
-                    .catch((error) => {
-                        console.log("error occur ", error);//handle this case
-                        ext_connection.send(JSON.stringify({
-                          'type':"join_response_error",
-                          'id':id,
-                          m:error.toString()
-                      }));
-                        //cb(error.toString())
-                    });
-                  break;
-          
-                default:
-                  if (mediaPeer) {
-                    mediaPeer.receiveRequest(request)
-                      .then((response) =>{ 
-                        ext_connection.send(JSON.stringify({
-                            'type':'response',
-                            'id':id,
-                            'm':response
-                        }));
-                          //cb(null, response)
-                      })
-                      .catch((error) => {
-                          console.log("error", error);
-                          ext_connection.send(JSON.stringify({
-                            'type':"response_error",
-                            'id':id,
-                            m:error.toString()
-                        }));
-                       //cb(error.toString())
-                      });
-                  }
-              }
+            media_soup_server.handle_media_soup_request(peer, request, what, id);
         }
         else if(response.type == 'request_close'){
-          //mediaPeer = room.getPeerByName(peerName);
-          console.log('request_close received...');
-          if(mediaPeer != null)
-            mediaPeer.close();
-          mediaPeer = null;
+          media_soup_server.handle_close(peer);
         }
         else if(response.type == 'mediasoup-notification'){
-          console.debug('Got notification from client peer', response.m);
-          if (!mediaPeer) {
-            console.error('Cannot handle mediaSoup notification, no mediaSoup Peer');
-            return;
-          }
-          
-          mediaPeer.receiveNotification(response.m);
+          media_soup_server.on_notification(response.m, peer);
         }
-    }
+     }
     );
 }
-
 
 wsServer.start(config.server.port, handle_conference);
--- server/media_soup_server.js
+++ server/media_soup_server.js
@@ -1,6 +1,7 @@
 const mediasoup = require('mediasoup');
 const config = require('./config');
 
+
 // // MediaSoup server
 const mediaServer = mediasoup.Server({
     numWorkers: null, // Use as many CPUs as available.
@@ -15,42 +16,227 @@ const mediaServer = mediasoup.Server({
   });
 
 
-  module.exports.create_room = codecs=>{
-    return mediaServer.Room(codecs);
+  function create_room(codecs){
+      return mediaServer.Room(codecs);
+  }
+
+  module.exports.create_room = create_room;
+
+class _room_handler{
+    constructor(){
+     this.rooms = new Map();
+    }
+ 
+    is_room_exists(roomId){
+      return this.rooms.has(roomId);
+    }
+ 
+    get_room_handle(roomId){
+      return this.rooms.get(roomId);
+    }
+
+    save_room(roomId, room){
+      this.rooms.set(roomId, room);
+    }
+
+    delete_room(roomId){
+      this.rooms.delete(roomId);
+    }
+ 
   };
 
-  module.exports.handleMediaPeer = (mediaPeer,con) => {
-    mediaPeer.on('notify', (notification) => {
-      console.log('New notification for mediaPeer received:', notification);
-      con.send(JSON.stringify({type:'mediasoup-notification', m:notification}));
-    });
+  class _peer{
+      constructor(key, name, roomId, con){
+          this.id_ = key;
+          this.peer_ = null;
+          this.name_ = name;
+          this.roomId_ = roomId;
+          this.con_ = con;
+      }
+      save_peer(peer){
+          this.peer_ = peer;
+      }
 
-    mediaPeer.on('newtransport', (transport) => {
-      console.log('New mediaPeer transport:', transport.direction);
-      transport.on('close', (originator) => {
-        console.log('Transport closed from originator:', originator);
-      });
-    });
+      get_peer(){ return this.peer_;}
 
-    mediaPeer.on('newproducer', (producer) => {
-      console.log('New mediaPeer producer:', producer.kind);
-      producer.on('close', (originator) => {
-        console.log('Producer closed from originator:', originator);
-      });
-    });
+      get_name(){return this.name_;}
+      get_roomId(){return this.roomId_;}
+      get_conection(){return this.con_;}
+  }
+ 
+ let room_handler = new _room_handler();
 
-    mediaPeer.on('newconsumer', (consumer) => {
-      console.log('New mediaPeer consumer:', consumer.kind);
-      consumer.on('close', (originator) => {
-        console.log('Consumer closed from originator', originator);
-      });
-    });
+ function handle_room_join_request(peerName, roomId){
 
-    // Also handle already existing Consumers.
-    mediaPeer.consumers.forEach((consumer) => {
-      console.log('mediaPeer existing consumer:', consumer.kind);
-      consumer.on('close', (originator) => {
-        console.log('Existing consumer closed from originator', originator);
+    if (room_handler.is_room_exists(roomId)) {
+      console.log("existing room found for request from "+ peerName);
+      return room_handler.get_room_handle(roomId);
+    } else {
+      console.log("creating new room for "+ peerName)
+      //room = mediaServer.Room(config.mediasoup.mediaCodecs);
+      let room = create_room(config.mediasoup.mediaCodecs);
+      room_handler.save_room(roomId, room);
+      room.on('close', () => {
+        room_handler.delete_room(roomId);
       });
-    });
+      return room;
+    }
+}
+
+ module.exports.handle_room_join_request = handle_room_join_request;
+
+function process_room_join(roomId, peerName, signaller){
+    if(room_handler.is_room_exists(roomId)){
+        let room = room_handler.get_room_handle(roomId);
+        let mediaPeer = room.getPeerByName(peerName); //todo: correct this
+
+        mediaPeer.on('notify', (notification) => {
+            console.log('New notification for mediaPeer received:', notification);
+            signaller.send(JSON.stringify({type:'mediasoup-notification', m:notification}));
+          });
+      
+          mediaPeer.on('newtransport', (transport) => {
+            console.log('New mediaPeer transport:', transport.direction);
+            transport.on('close', (originator) => {
+              console.log('Transport closed from originator:', originator);
+            });
+          });
+      
+          mediaPeer.on('newproducer', (producer) => {
+            console.log('New mediaPeer producer:', producer.kind);
+            producer.on('close', (originator) => {
+              console.log('Producer closed from originator:', originator);
+            });
+          });
+      
+          mediaPeer.on('newconsumer', (consumer) => {
+            console.log('New mediaPeer consumer:', consumer.kind);
+            consumer.on('close', (originator) => {
+              console.log('Consumer closed from originator', originator);
+            });
+          });
+      
+          // Also handle already existing Consumers.
+          mediaPeer.consumers.forEach((consumer) => {
+            console.log('mediaPeer existing consumer:', consumer.kind);
+            consumer.on('close', (originator) => {
+              console.log('Existing consumer closed from originator', originator);
+            });
+          });
+          return mediaPeer;
+    }
+    else{
+        //todo: it should not return hre.
+    }
+ };
+
+ module.exports.process_room_join = process_room_join;
+
+ 
+ function check_peer_media(mediaPeer, request, id, signaller){
+    if(mediaPeer == null) return;
+    if (mediaPeer) {
+        mediaPeer.receiveRequest(request)
+          .then((response) =>{ 
+            signaller.send(JSON.stringify({
+                'type':'response',
+                'id':id,
+                'm':response
+            }));
+              //cb(null, response)
+          })
+          .catch((error) => {
+              console.log("error", error);
+              signaller.send(JSON.stringify({
+                'type':"response_error",
+                'id':id,
+                m:error.toString()
+            }));
+           //cb(error.toString())
+          });
+      }
+ }
+  
+  function handle_media_soup_request(peer, request, what, id){
+    let roomId =  peer.get_roomId();
+    let signaller = peer.get_conection();
+   let room = room_handler.get_room_handle(roomId);
+   let peerName = peer.get_name();
+    console.log(request, what);
+    switch (what) {
+  
+        case 'queryRoom':
+          room.receiveRequest(request)
+            .then((response) => {
+                signaller.send(JSON.stringify(
+                    {'type':"queryRoomResponse",
+                      'id':id,
+                      'm':response
+                    }));
+            })
+            .catch((error) =>{
+                console.log("handle this error", error);
+                signaller.send(JSON.stringify(
+                    {'type':"queryRoomResponseError",
+                      'id':id,
+                      'm':error.toString()
+                    }));
+            });
+          break;
+  
+        case 'join':
+          room.receiveRequest(request)
+            .then((response) => {
+                let mediaPeer = process_room_join(roomId, peerName,signaller);
+                peer.save_peer(mediaPeer);
+                signaller.send(JSON.stringify({
+                    'type':"join_response",
+                    'id':id,
+                    m:response
+                }));
+
+            })
+            .catch((error) => {
+                console.log("error occur ", error);//handle this case
+                signaller.send(JSON.stringify(
+                    {'type':"join_response_error",
+                      'id':id,
+                      'm':error.toString()
+                    }));
+            });
+          break;
+  
+        default:
+        check_peer_media(peer.get_peer(), request, id, signaller);
+      }
+  }
+
+  module.exports.handle_media_soup_request = handle_media_soup_request;
+
+  function handle_close(peer){
+    console.log('request_close received...');
+
+    let mediaPeer = peer.get_peer();
+    if(mediaPeer != null)
+      mediaPeer.close();
+  }
+
+  module.exports.handle_close = handle_close;
+
+  function handle_notification(msg, peer){
+    console.debug('Got notification from client peer', msg);
+    let mediaPeer = peer.get_peer();
+    if (!mediaPeer) {
+      console.error('Cannot handle mediaSoup notification, no mediaSoup Peer');
+      return;
+    }
+    
+    mediaPeer.receiveNotification(msg);
+  }
+
+  module.exports.on_notification = handle_notification;
+
+  module.exports.save_peer = (key, peerName, roomId, con)=>{
+    let peer = new _peer(key, peerName, roomId, con);
+    return peer;
   }
