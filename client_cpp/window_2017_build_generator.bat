52096a8e779c62ffca47a9610b013010530d5d4b
anil235476 <aks.dce.007@gmail.com>
2019-10-07 11:21:46 +0530

    adding secure flag

--- client_cpp/window_2017_build_generator.bat
+++ client_cpp/window_2017_build_generator.bat
@@ -1,1 +1,1 @@
-cmake ../ -G "Visual Studio 15 2017 Win64" -DLIBWEBRTC_INCLUDE_PATH:PATH="D:/webrtcBuild/Sept/webrtc_checkout/src" -DLIBWEBRTC_BINARY_PATH:PATH="D:/git_project/webRTC_client/webrtc_open_source/debug/webrtc_open_source/debug"
+cmake ../ -G "Visual Studio 15 2017 Win64" -DSECURE_SIGNALING=OFF -DLIBWEBRTC_INCLUDE_PATH:PATH="D:/webrtcBuild/Sept/webrtc_checkout/src" -DLIBWEBRTC_BINARY_PATH:PATH="D:/git_project/webRTC_client/webrtc_open_source/debug/webrtc_open_source/debug"
